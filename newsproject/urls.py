f25940664df9e8c473797c1a418c364b11d5c2e2
ArtemKhymenko <breakerhim@ua.fm>
2016-07-16 20:11:11 +0300

    Update urls.py

--- newsproject/urls.py
+++ newsproject/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^news/', include('news.urls')),
+    url(r'^', include('news.urls')),
 ]
